6236ac916fe669b649d390164d577b3db6afcf7d
Raltour <15610159165lmz@gmail.com>
2025-06-03 14:28:41 +0800

    做了一点proj1，遇到神秘bug，先放一放

--- Project/proj1/imageloader.c
+++ Project/proj1/imageloader.c
@@ -26,16 +26,59 @@
 Image *readData(char *filename) 
 {
 	//YOUR CODE HERE
+	Image *image =  (Image*)malloc(sizeof(Image));
+	if (image == NULL) {
+		printf("Error allocating memory\n");
+		return NULL;
+	}
+
+	FILE *fp = fopen(filename, "r");
+	char buff[3];
+	fscanf(fp, "%s", buff);
+	int row, col;
+	fscanf(fp, "%d %d", &col, &row);
+	int maxcolor;
+	fscanf(fp, "%d", &maxcolor);
+
+	image->image = (Color**)malloc(sizeof(Color*) * row);
+	for(int i = 0; i < row; i++) {
+		image->image[i] = (Color*)malloc(sizeof(Color) * col);
+	}
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			fscanf(fp, "%d %d %d", image->image[i][j].R, image->image[i][j].G, image->image[i][j].B);
+		}
+	}
+	image->cols = col;
+	image->rows = row;
+	fclose(fp);
+	return image;
 }
 
 //Given an image, prints to stdout (e.g. with printf) a .ppm P3 file with the image's data.
 void writeData(Image *image)
 {
 	//YOUR CODE HERE
+	printf("P3\n");
+	printf("%d %d\n", image->cols, image->rows);
+	printf("255\n");
+	for (int i = 0; i < image->rows; i++) {
+		int j;
+		for (j = 0; j < image->cols - 1; j++) {
+			printf("%3d %3d %3d   ", image->image[i][j].R, image->image[i][j].G, image->image[i][j].B);
+		}
+		printf("%3d %3d %3d", image->image[i][j].R, image->image[i][j].G, image->image[i][j].B);
+		printf("\n");
+	}
 }
 
 //Frees an image
 void freeImage(Image *image)
 {
 	//YOUR CODE HERE
+	for (int i = 0; i < image->rows; i++) {
+		free(image->image[i]);
+	}
+	free(image->image);
+	free(image);
 }
