4b24e0374207d388b95d7de4edeee9da299f3bb8
Raltour <15610159165lmz@gmail.com>
2025-06-08 09:59:51 +0800

    完成了ex3 实现 factorial 函数的递归逻辑 ex1和ex2也已经做完了

--- Lab/lab03/factorial.s
+++ Lab/lab03/factorial.s
@@ -14,7 +14,7 @@ main:
     ecall # Print Result
 
     addi a1, x0, '\n'
-    addi a0, x0, 11
+    addi a0, x0, 1
     ecall # Print newline
 
     addi a0, x0, 10
@@ -22,3 +22,19 @@ main:
 
 factorial:
     # YOUR CODE HERE
+    bne a0, x0, recusion
+    addi a0, a0, 1
+    j return
+recusion: 
+    addi sp, sp, -8 
+    sw s0, 0(sp)
+    sw ra, 4(sp)
+    mv s0, a0
+    addi a0, a0, -1
+    jal ra, factorial
+    mul a0, a0, s0
+    lw s0, 0(sp)
+    lw ra, 4(sp)
+    addi sp, sp, 8
+return:
+    jr ra
